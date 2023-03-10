481595ac4a9a2f79f5eea373c6bb08c56a42df31
muhammadashir123 <ashir@genesisengr.com>
2023-03-11 00:50:22 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import logo from './logo.svg';
+import Heading from './heading';
+import './App.css';
+import useReactRouter from 'use-react-router';
+import { useState } from 'react';
+
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button>I'm a button</button>
+  );
+}
+export default function MyApp() {
+  const handleUpClick=()=>{
+    // let newText= text.toUpperCase();
+    // setText(newText)
+  }
+
+
+  let arr = [1,2,3,4]
+  //const [text, setText]= useState(arr);
+const [first, setArr]=useState(arr)
+  const addNumber=()=>{
+    setArr(...first)
+
+  }
+console.log([...first])
+  return (
+    <div>
+    <br></br>
+    {/* <textarea   rows="4" cols="50" value={text}>
+At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+</textarea> */}
+<button onClick={handleUpClick}>convert to upercase</button>
+<button onClick={addNumber}>addNumber</button> 
+         
+         <ul>
+             { first &&
+              first.map((num) => {
+                return <li key={num}>{num}</li>
+              })
+             }
+         </ul>
+
+<br></br>
+  
     </div>
   );
 }
 
-export default App;
+
---
+++ src/heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Heading(){
+    return(
+        <>
+        <navbar title="letter" desciption="jobdescription"></navbar>
+        </>
+      
+    )
+}
